0128a9ecd41d22c79ca4d28c8798408257c6f695
James Ward <james@notjam.es>
2020-09-15 00:40:13 -0400

    FindCriteriaNotFoundError must extend EntityColumnNotFound

--- src/error/FindCriteriaNotFoundError.ts
+++ src/error/FindCriteriaNotFoundError.ts
@@ -1,12 +1,13 @@
 import {EntityMetadata} from "..";
+import {EntityColumnNotFound} from "./EntityColumnNotFound";
 
 /**
  * Thrown when specified entity property in the find options were not found.
  */
-export class FindCriteriaNotFoundError extends Error {
+export class FindCriteriaNotFoundError extends EntityColumnNotFound {
 
     constructor(propertyPath: string, metadata: EntityMetadata) {
-        super();
+        super(propertyPath);
         Object.setPrototypeOf(this, FindCriteriaNotFoundError.prototype);
         this.message = `Property "${propertyPath}" was not found in ${metadata.targetName}. Make sure your query is correct.`;
     }
